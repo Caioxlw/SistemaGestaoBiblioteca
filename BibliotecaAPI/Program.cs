1e4e0dda830a800b13a2bf143a8373be780fa43b
Caio <caioxlw@gmail.com>
2026-08-14 16:36:25 -0300

    feat(api): configurar inicialização da aplicação

--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -1,20 +1,34 @@
 using BibliotecaAPI.Data;
 using BibliotecaAPI.Middlewares;
+using BibliotecaAPI.Repositories;
+using BibliotecaAPI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Configuração do DbContext com SQLite
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<BibliotecaDbContext>(
-    options => options.UseSqlite(connectionString)
-    );
+builder.Services.AddDbContext<BibliotecaDbContext>(options => 
+    options.UseSqlite(connectionString));
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+// Injeção de Dependência - Repositories
+builder.Services.AddScoped<IAutorRepository, AutorRepository>();
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
+builder.Services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
+
+// Injeção de Dependência - Services
+builder.Services.AddScoped<IAutorService, AutorService>();
+builder.Services.AddScoped<ILivroService, LivroService>();
+builder.Services.AddScoped<IAlunoService, AlunoService>();
+builder.Services.AddScoped<IEmprestimoService, EmprestimoService>();
+
 var app = builder.Build();
 
+// Middleware Global de Tratamento de Erros
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
@@ -25,28 +39,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
